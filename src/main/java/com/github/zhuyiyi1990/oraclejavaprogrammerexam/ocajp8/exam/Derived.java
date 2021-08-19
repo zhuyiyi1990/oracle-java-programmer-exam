cad9b0a1bfd5554729a6ac313e5a5ae9e1ead19d
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-08-19 16:42:58 +0800

    添加ocajp8考试时遇到的题目

---
+++ src/main/java/com/github/zhuyiyi1990/oraclejavaprogrammerexam/ocajp8/exam/Derived.java
@@ -0,0 +1,22 @@
+package com.github.zhuyiyi1990.oraclejavaprogrammerexam.ocajp8.exam;
+
+abstract class Base {
+
+    protected void f1() {
+
+    }
+
+    abstract void f2();
+
+}
+
+public class Derived extends Base {
+
+    protected void f1() {
+
+    }
+
+    void f2() {
+
+    }
+}
